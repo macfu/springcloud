71ddb52706dd3bbff74ee714072094dad967a98b
liming <v_fuliming@baidu.com>
2018-12-05 11:15:42 +0800

    add base springSecurity

--- microcloud-consumer-80/src/main/java/com/macfu/config/RestConfig.java
+++ microcloud-consumer-80/src/main/java/com/macfu/config/RestConfig.java
@@ -2,7 +2,11 @@ package com.macfu.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.client.RestTemplate;
+
+import java.nio.charset.Charset;
+import java.util.Base64;
 
 /**
  * @Author: liming
@@ -14,5 +18,23 @@ public class RestConfig {
     @Bean
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
+    }
+
+    /**
+     * 在provider-dept-8001项目中配置了SpringSecurity，所以访问rest服务时需要在头信息上添加配置认证信息
+     * @return 包含有认证信息的HttpHeaders
+     */
+    @Bean
+    public HttpHeaders getHeaders() {
+        // 定义一个Http的头信息
+        HttpHeaders headers = new HttpHeaders();
+        // 认证的原始信息
+        String auth = "macfu:hello";
+        // 进行加密处理
+        byte[] encode = Base64.getEncoder().encode(auth.getBytes(Charset.forName("US-ASCII")));
+        // 在进行授权的头信息内容配置的时候加密的信息一定要与"Basic"之间有一个空格
+        String authHeader = "Basic " + new String(encode);
+        headers.set("Authorization", authHeader);
+        return headers;
     }
 }
--- microcloud-consumer-80/src/main/java/com/macfu/controller/DeptController.java
+++ microcloud-consumer-80/src/main/java/com/macfu/controller/DeptController.java
@@ -1,6 +1,9 @@
 package com.macfu.controller;
 
 import com.macfu.po.Dept;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -20,25 +23,54 @@ public class DeptController {
     public static final String DEPT_LIST_URL = "http://localhost:8001/dept/list";
     public static final String DEPT_ADD_URL = "http://172.21.217.26:8001/dept/add";
 
+//    @Resource
+//    private RestTemplate restTemplate;
+//
+//    @RequestMapping("/get")
+//    public Object getDept(long id) {
+//        Dept dept = this.restTemplate.getForObject(DEPT_GET_URL + id, Dept.class);
+//        return dept;
+//    }
+//
+//    @RequestMapping("/list")
+//    public Object listDept() {
+//        System.out.println("123242424");
+//        List<Dept> allDepts = this.restTemplate.getForObject(DEPT_LIST_URL, List.class);
+//        return allDepts;
+//    }
+//
+//    @RequestMapping("/add")
+//    public Object addDept(Dept dept) {
+//        Boolean flag = this.restTemplate.postForObject(DEPT_ADD_URL, dept, Boolean.class);
+//        return flag;
+//    }
+
+
+    /**
+     * 加入SpringSecurity之后的消费端
+     */
     @Resource
     private RestTemplate restTemplate;
+    @Resource
+    private HttpHeaders headers;
 
     @RequestMapping("/get")
     public Object getDept(long id) {
-        Dept dept = this.restTemplate.getForObject(DEPT_GET_URL + id, Dept.class);
+        Dept dept = this.restTemplate.exchange(DEPT_GET_URL + id, HttpMethod.GET, new HttpEntity<Object>(this.headers), Dept.class).getBody();
         return dept;
     }
 
     @RequestMapping("/list")
     public Object listDept() {
         System.out.println("123242424");
-        List<Dept> allDepts = this.restTemplate.getForObject(DEPT_LIST_URL, List.class);
+        List<Dept> allDepts = this.restTemplate.exchange(DEPT_LIST_URL, HttpMethod.GET, new HttpEntity<Object>(this.headers), List.class).getBody();
         return allDepts;
     }
 
     @RequestMapping("/add")
-    public Object addDept(Dept dept) {
-        Boolean flag = this.restTemplate.postForObject(DEPT_ADD_URL, dept, Boolean.class);
+    public Object addDept(Dept dept) throws Exception {
+        Boolean flag = this.restTemplate.exchange(DEPT_ADD_URL, HttpMethod.GET, new HttpEntity<Object>(dept, this.headers), Boolean.class).getBody();
         return flag;
     }
+
 }
